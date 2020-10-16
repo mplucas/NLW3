f04b448f175e0a3ee5b81edfa1ac9bf4c2121684
Lucas Meneghelli Pereira <42855038+mplucas@users.noreply.github.com>
2020-10-16 03:00:20 -0300

    - Finished 3rd class: intregrations o front with back ends.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 
 import './styles/global.css';
+import 'leaflet/dist/leaflet.css';
 
 import Routes from './routes';
-
-interface TitleProps {
-  index:string;
-}
-
-function Title(props:TitleProps) {
-  return <h1>Hello World {props.index}</h1>
-}
 
 function App() {
   return (
